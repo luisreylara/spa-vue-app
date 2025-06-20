20ec2309a49cf2f10ef72f06e1a638080c5d6e15
Luis <luisreylara@hotmail.com>
2025-06-20 10:59:39 -0600

    primer commit video 1-12

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import HomePage from '@/modules/landing/pages/HomePage.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+export const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/features',
+      name: 'features',
+      component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+      component: () => import('@/modules/landing/pages/PricingPage.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/modules/landing/pages/ContactPage.vue'),
+    },
+  ],
+});
+export default router;
